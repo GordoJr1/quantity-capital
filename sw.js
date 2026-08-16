17723789129716d915aa966d7857a9bad9e7091b
GordoJr1 <gordojr@proton.me>
2026-08-16 19:59:03 -0400

    Use the chart-Q mark as the home-screen icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "qc-shell-v1";
+const CACHE = "qc-shell-v2";
 const SHELL = [
   "./",
   "./index.html",
@@ -7,6 +7,7 @@ const SHELL = [
   "./city.jpg?v=15",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
+  "./icons/icon-512-maskable.png",
   "./icons/apple-touch-icon.png"
 ];
 
